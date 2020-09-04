91171d79f23ee16e24673166aeabd899e90eb32f
Jwilson1172 <jw59615@gmail.com>
2020-09-04 19:34:38 -0400

    adding new auth header to the test cases and adding forbidden/allowed testing to test_main.py
    Signed-off-by: Jwilson1172 <jw59615@gmail.com>

--- project/app/tests/test_main.py
+++ project/app/tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-
+from os import getenv
 from app.main import app
 
 client = TestClient(app)
@@ -7,6 +7,18 @@ client = TestClient(app)
 
 def test_docs():
     """Return HTML docs for root route."""
-    response = client.get('/')
+    # set the authorization token so that the API accepts the connection
+    client.headers["Authorization"] = getenv("DS_SECRET_TOKEN", None)
+    # pull the content with from the root page (in this case swagger_ui)
+    response = client.get("/")
+    # check to make sure status_code is good and site is up
     assert response.status_code == 200
-    assert response.headers['content-type'].startswith('text/html')
+    # checks to make sure that the swagger_ui loads as html
+    assert response.headers["content-type"].startswith("text/html")
+
+    # change the headers to unauthorized
+    client.headers["Authorization"] = None
+    # pull the root page content again
+    response = client.get("/")
+    # should throw 401 forbidden
+    assert response.status_code == 403
