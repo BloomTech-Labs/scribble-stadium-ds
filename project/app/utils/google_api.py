0fda7c0d6f7a309240b4d8a852eae14d1f0e630a
Jwilson1172 <jw59615@gmail.com>
2020-09-15 11:17:49 -0400

    adding google api to a GoogleAPI sevice class

--- project/app/utils/google_api.py
+++ project/app/utils/google_api.py
@@ -1,36 +1,43 @@
 # Google Vision function to extract text from a local or uri hosted image
 
 from google.cloud import vision
+from google.cloud.vision import types
 import io
 
 
-def transcribe(image_path):
-    '''
-    Detects document features in images and returns extracted text
-    Input: Path to file where images are stored
-        - Assuming 1 image per image_path
-        - Code for both local image_path and remote image_path, comment out
-            the apporopriate one
-    Output: Transcribed text as a string
-    '''
+class GoogleAPI:
+    def __init__(self):
+        pass
 
-    # If image_path is local
-    with io.open(image_path, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.types.Image(content=content)
+    def transcribe(self, image_path):
+        """
+        Detects document features in images and returns extracted text
+        Input: Path to file where images are stored
+            - Assuming 1 image per image_path
+            - Code for both local image_path and remote image_path, comment out
+                the apporopriate one
+        Output: Transcribed text as a string
+        """
 
-    # # If image_path is a uri
-    # image = vision.types.Image()
-    # image.source.image_uri = uri
+        # If image_path is local
+        with io.open(image_path, "rb") as image_file:
+            content = image_file.read()
+        image = types.Image(content=content)
 
-    # Connect to Google API client
-    client = vision.ImageAnnotatorClient()
-    response = client.document_text_detection(image=image)
+        # # If image_path is a uri
+        # image = vision.types.Image()
+        # image.source.image_uri = uri
 
-    # Save transcribed text
-    if response.text_annotations:
-        transcribed_text = response.text_annotations[0].description.replace('\n', ' ')
-    else:
-        print('No Text Detected')
+        # Connect to Google API client
+        client = vision.ImageAnnotatorClient()
+        response = client.document_text_detection(image=image)
 
-    return transcribed_text
+        # Save transcribed text
+        if response.text_annotations:
+            transcribed_text = response.text_annotations[0].description.replace(
+                "\n", " "
+            )
+        else:
+            print("No Text Detected")
+
+        return transcribed_text
