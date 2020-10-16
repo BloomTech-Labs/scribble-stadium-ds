f89df558e3616a9ec76e9c20cc09a5d1111cb828
Lori Schlatter <lori.schlatter@gmail.com>
2020-10-16 18:15:46 -0500

    update clustering so that only clusters of 4 are returned

--- project/app/utils/clustering/clustering_mvp.py
+++ project/app/utils/clustering/clustering_mvp.py
@@ -7,9 +7,12 @@ def cluster(cohort_submissions: dict) -> list:
     '''
     Splits given dict into clusters of 4 based on their ranked complexity
 
-    When there is a remainder of users not evenly divisible by 4,
-    remainder is split so there is never more than 1 computer user in a group,
-    unless there are fewer than 3 users.
+    The 'remainder problem' of needing to have 4 submissions per cluster,
+    regardless of number of submissions, is solved here by duplicating
+    submission IDs across clusters to supplement any clusters with less than 4,
+    with no clusters containing more than 1 submission_ID that also
+    appears in another cluster, unless there are fewer than 3 total
+    submissions, or exactly 5 submissions, in the given cohort.
 
     Input: dictionary of a single cohort containing nested dictionary
     with 'submission_id' as first level key,
@@ -32,51 +35,65 @@ def cluster(cohort_submissions: dict) -> list:
 
     # Edge Cases:
     # - less than 4, they are all in one group
-    # - 5, one group of 3 one group of 2
     if num_submissions < 4:
-        clusters.append(list(df.index[:]))
-
-    elif num_submissions == 5:
-        clusters.append(list(df.index[:3]))
-        clusters.append(list(df.index[3:]))
+        return "Not enough submissions in this cohort to form a full cluster"
 
     # If the remainder is 3 -> last group will be a group of 3 users
     elif remainder == 3:
-        for i in range(num_clusters):
-            # Group by top 4 squad scores
-            clusters.append(list(df.index[:4]))
-            # Drop stories you have grouped already
-            df = df[4:]
-
-        # Final group is the last 3 remainders
-        clusters.append(list(df.index[:]))
-
-    # If the remainder is 2 -> last 2 groups will be groups of 3
-    elif remainder == 2:
-        # Leave the last 2 groups to split into 2 groups of 3
+        # Cluster submissions until 7 remain
         for i in range(num_clusters - 1):
             # Group by top 4 squad scores
             clusters.append(list(df.index[:4]))
             # Drop stories you have grouped already
             df = df[4:]
 
-        # The last two groups will be groups of 3
-        clusters.append(list(df.index[:3]))
+        # Manually cluster final two groups to handle remainder problem
+        # by duplicating 1 submission so each have 4 submission_ids
+        clusters.append(list(df.index[:4]))
         clusters.append(list(df.index[3:]))
+
+    # If the remainder is 2 -> last 2 groups will be groups of 3
+    elif remainder == 2:
+        if num_submissions == 6:
+            clusters.append(df.index[:4])
+            clusters.append(df.index[2:])
+        else:
+            # Cluster submissions until 10 remain
+            for i in range(num_clusters - 2):
+                # Group by top 4 squad scores
+                clusters.append(list(df.index[:4]))
+                # Drop stories you have grouped already
+                df = df[4:]
+
+            # Manually cluster final three groups to handle remainder problem
+            clusters.append(list(df.index[:4]))
+            clusters.append(list(df.index[3:7]))
+            clusters.append(list(df.index[6:]))
 
     # If the remainder is 1 -> last 3 groups will be groups of 3
     elif remainder == 1:
-        # Leave the last 3 groups to be split into 3 groups of 3
-        for i in range(num_clusters - 2):
-            # Group by top 4 squad scores
+        if num_submissions == 5:
+            # Overlap both clusters by 3 submissions
             clusters.append(list(df.index[:4]))
-            # Drop stories you have already grouped
-            df = df[4:]
+            clusters.append(list(df.index[1:]))
+        elif num_submissions == 9:
+            clusters.append(list(df.index[:4]))
+            clusters.append(list(df.index[2:6]))
+            clusters.append(list(df.index[5:]))
+        else:
+            # Cluster submissions until 13 remain
+            for i in range(num_clusters - 3):
+                # Group by top 4 squad scores
+                clusters.append(list(df.index[:4]))
+                # Drop stories you have already grouped
+                df = df[4:]
 
-        # The last three groups as groups of 3
-        clusters.append(list(df.index[:3]))
-        clusters.append(list(df.index[3:6]))
-        clusters.append(list(df.index[6:]))
+            # Manually cluster final three overlapping groups to handle
+            # remainder problem
+            clusters.append(list(df.index[:4]))
+            clusters.append(list(df.index[3:7]))
+            clusters.append(list(df.index[6:10]))
+            clusters.append(list(df.index[9:]))
 
     # Else, the remainder is 0. Split evenly by 4
     else:
