ede50eac2394bca015ba298d5e27a10ecb1ee294
Jwilson1172 <jw59615@gmail.com>
2020-10-19 16:56:49 -0400

    adding visualization endpoints

--- project/app/api/visualization.py
+++ project/app/api/visualization.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 
 from app.utils.security.header_checking import AuthRouteHandler
 from app.api.models import HistogramRequest, LineGraphRequest
+from app.utils.visualizations import histogram, line_graph
 
 # global variables and services
 router = APIRouter(route_class=AuthRouteHandler)
@@ -12,9 +13,11 @@ log = logging.getLogger(__name__)
 
 @router.route("/viz/linegraph")
 def return_line_graph(data: LineGraphRequest):
-    pass
+    return line_graph.line_graph(data.ScoreHistory, data.StudentName)
 
 
 @router.route("/viz/histogram")
 def return_histogram(data: HistogramRequest):
-    pass
+    return histogram.histogram(
+        data.GradeList, [data.GradeLevel, data.StudentName, data.StudentScore]
+    )
