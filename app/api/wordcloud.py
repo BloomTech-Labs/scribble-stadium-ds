b41b3f5f1a4fd7cab537842ff122d247c8397111
TemsyChen <70554318+TemsyChen@users.noreply.github.com>
2021-07-22 23:21:23 -0700

    API successfully takes in a story string and outputs complex words

--- app/api/wordcloud.py
+++ app/api/wordcloud.py
@@ -4,10 +4,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-class ComplexWords(BaseModel):
-    """The submission entry in string form"""
-    text: str
-
 def _syllables(word):
     syllable_count = 0
     vowels = 'aeiouyAEIOUY'
@@ -33,55 +29,45 @@ def clean_text(story):
     #tokenize words
     story = story.split(sep=' ')
 
+    #remove '' from list
+    story = [x for x in story if x != '']
+
     return story
 
 @router.post('/wordcloud/text')
 async def wordcloudtext(story):
 
-    words = clean_text(story)
+    story_words = clean_text(story)
 
+    # Create a dictionary to count occurences of words
     word_counts = dict()
-    for word in words:
+    for word in story_words:
         if word in word_counts:
             word_counts[word] += 1
         else:
             word_counts[word] = 1
 
-    # # put this in a dataframe
-    # # make a column of word counts
-    # words = pd.DataFrame({'word': word_counts.keys(), 
-    #                      'count': word_counts.values()})
-
-    # # make a column for letter counts
-    # words['len'] = words['word'].apply(len)
-
-    # # column for syllable count
-    # words['syllables'] = words['word'].apply(_syllables)
-    # # https://medium.com/@mholtzscher/programmatically-counting-syllables-ca760435fab4
-
-    # # make a column for how BIG a word is
-    # words['complexity'] = words['syllables'] + words['len']
-
-    # res = set(words.loc[words['complexity'] > 7, 'word'])
-   
-    # return res
-    complex_word = []
+    # Convert the dictionary to a dataframe
+    word_list = []
+    count_list = []
 
     for k, v in word_counts.items():
-        if v > 1:
-            complex_word.append(k)
-    
-    return complex_word
+        word_list.append(k)
+        count_list.append(v)
 
+    words = pd.DataFrame({'word': word_list, 'count': count_list})
 
+    # make a column for letter counts
+    words['len'] = words['word'].apply(len)
 
- 
+    # column for syllable count
+    words['syllables'] = words['word'].apply(_syllables)
+    # https://medium.com/@mholtzscher/programmatically-counting-syllables-ca760435fab4
 
+    # make a column for how BIG a word is
+    words['complexity'] = words['syllables'] + words['len']
 
+    # Return the words that have a complexity score >= 7
+    res = list(words.loc[words['complexity'] >= 7, 'word'])
 
-    # # make a column for how BIG a word is
-    # words['complexity'] = words['syllables'] + words['len']
-
-    # complex_words = list(words.loc[words['len'] >= 3, 'word'])
-
-    # return complex_words
+    return res
