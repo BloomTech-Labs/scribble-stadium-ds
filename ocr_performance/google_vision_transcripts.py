7f46f599701df4a769924ac01d6842f8da2fe60d
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-07-23 20:39:39 -0700

    Fully functioning google_vision_transcripts.py file - have successfully generated Google Vision transcripts for all image files except 5238-2 which does not contain any text

--- ocr_performance/google_vision_transcripts.py
+++ ocr_performance/google_vision_transcripts.py
@@ -1,10 +1,9 @@
 import logging
+from os import listdir
 from os.path import dirname
 from asyncio import get_event_loop
 
 from app.utils.img_processing.google_api import GoogleAPI, NoTextFoundException
-
-
 # global variables and services
 DIR = dirname(__file__)
 DATA_DIR = DIR + '/../data/'
@@ -14,8 +13,6 @@ METADATA_DIR = DATA_DIR + 'metadata/'
 GOOGLE_TRANSCRIPTS_DIR = DATA_DIR + 'google_transcripts/'
 
 vision = GoogleAPI()
-
-
 
 async def transcribe(page_image_file):
 
@@ -27,14 +24,32 @@ async def transcribe(page_image_file):
     with open(page_image_file, mode='rb') as pifd:
         page_image = pifd.read()
 
-    low_confidence, flagged, trans_text = await vision.transcribe(page_image)
-
+    low_confidence = True
+    flagged = True
+    trans_text = 'No Text Found'
+    try:
+        low_confidence, flagged, trans_text = await vision.transcribe(page_image)
+    except NoTextFoundException:
+         print('No Text Found')
+         
     with open(meta_file_name,'w') as metafd:
         metafd.write(' '.join([str(low_confidence), str(flagged)]))
     with open(trans_file_name,'w') as transfd:
         transfd.write(trans_text)
-        
-    
 
-loop = get_event_loop()
-loop.run_until_complete(transcribe(PHOTOS_DIR+'3101.jpg'))
+        
+        
+
+async def transcribe_all():
+    
+    image_files = [ x for x in listdir(PHOTOS_DIR) if x.endswith('.jpg')]
+    image_files.sort()
+    
+    for idx,image_file in enumerate(image_files):
+        print(f'Processing image<{idx}:{image_file}>')
+        await transcribe(PHOTOS_DIR+image_file)
+
+if __name__ == '__main__':
+    loop = get_event_loop()
+    loop.run_until_complete(transcribe_all())
+
