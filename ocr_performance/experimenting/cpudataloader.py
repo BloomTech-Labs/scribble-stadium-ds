6afd00fb6810bac6033c3f64e073f1978190b170
Tasha Upchurch <TashaSkyUp@gmail.com>
2021-09-14 19:24:17 -0400

    changed data_path to be based from where the .py file is, avoides conflicts with different interpreter location setups

--- ocr_performance/experimenting/cpudataloader.py
+++ ocr_performance/experimenting/cpudataloader.py
@@ -2,9 +2,9 @@
 cpu_data_loader.py - Loads all data in the ../../data/transcribed_stories directory
 """
 import pathlib
-import os
+import os.path as path
 
 
 class CPUDataLoader():
     def __init__(self):
-        self.data_path = os.path.join("..", "..", "data", "transcribed_stories")
+        self.data_path = path.join(path.dirname(__file__),"..", "..", "data", "transcribed_stories")
