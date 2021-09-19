c78b62adb68bd547a5ca6ee97d477587e45c49b2
singparvi <sing.parvi1@gmail.com>
2021-09-18 22:30:42 -0400

    new .py that runs tkinter app to convert image to Grayscale

---
+++ data_managment/story_photo_GUI_grayscale.py
@@ -0,0 +1,116 @@
+"""
+This module's purpose is to open the image in it's original format and display through
+a tkinter application and further enable the user to see the same image in a
+grayscale format. Finally the image can be saved in the same directory
+as that of the image as grayscale.
+
+image will be saved with _grayscale appended before the file extension
+"""
+
+import numpy as np
+import os.path as path
+import tkinter as tk
+from tkinter import filedialog as fd
+import cv2
+
+
+def np_photo_image(image: np.ndarray):
+    height, width, channels = image.shape
+    data = f'P6 {width} {height} 255 '.encode() + image.astype(np.uint8).tobytes()
+    return tk.PhotoImage(width=width, height=height, data=data, format='PPM')
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+
+        self.filename = fd.askopenfilename(
+            initialdir=path.join(path.dirname(__file__), "..", "data", "transcribed_stories", "51--", "5101"))
+        self.np_img = np.array(cv2.cvtColor(cv2.imread(self.filename), cv2.COLOR_RGB2BGR))
+        self.img = np_photo_image(self.np_img)
+
+        # Convert image to grayscale
+        self.np_img_grayscale = np.array(cv2.cvtColor(cv2.imread(self.filename), cv2.COLOR_BGR2GRAY))
+
+        self.points = []
+        self.cursor_oval_handles = []
+        self.line_handles = []
+        self.create_widgets()
+        self.newest_pt_idx = -1
+        # self.cursor
+
+        print(self.filename)
+
+    def create_widgets(self):
+        self.show_as_grayscale = tk.Button(self)
+        self.show_as_grayscale["text"] = "Show as Grayscale"
+        self.show_as_grayscale["command"] = self.show_as_grayscale_button
+        self.show_as_grayscale.pack(side="top")
+
+        # Save Button for Gray Scale
+        self.save_btn_grayscale = tk.Button(self)
+        self.save_btn_grayscale["text"] = "Save as GrayScale"
+        self.save_btn_grayscale["command"] = self.save_button_grayscale
+        self.save_btn_grayscale.pack(side="top")
+
+        # Quit Button
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+        # canvas
+        self.canvas = tk.Canvas()
+        self.canvas.pack(fill="both", expand=True)
+        self.canvas.create_image(8, 8, anchor=tk.NW, image=self.img)
+        self.canvas.bind('<Configure>', self.resize)
+        # self.canvas.bind("<Button-1>", self.canvas_click)
+        # self.canvas.bind("<Motion>", self.canvas_mouseover)
+
+        self.image_handle = None
+
+    def save_button_grayscale(self):
+        """
+        Save Button Grayscale to save file as Grayscale in file path directory
+        :return: None
+        """
+        directory = path.dirname(self.filename)
+        filename, extension = path.basename(self.filename).split(".")
+        new_file_name = path.join(directory, filename + "-grayscale" + "." + extension)
+        cv2.imwrite(new_file_name, self.np_img_grayscale)
+        print('File saved as grayscale, path -->', new_file_name)
+
+    def show_as_grayscale_button(self):
+        """
+        Transform Button to open image as Grayscale
+        :return: None
+        """
+        print('Button Pressed')
+        # Get CV to convert the image to GrayScale
+        self.image_grayscale = cv2.imread(self.filename)
+        self.gray = cv2.cvtColor(self.image_grayscale, cv2.COLOR_BGR2GRAY)
+        self.resized = cv2.resize(self.gray, (800, 1000))
+        cv2.imshow('Gray image (press any key to exit)', self.resized)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def resize(self, event):
+        w = self.canvas.winfo_height()
+        h = self.canvas.winfo_width()
+        self.img = np_photo_image(cv2.resize(self.np_img, (h, w)))
+
+        if not self.image_handle:
+            self.image_handle = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+        else:
+            # if (self.img.width() != w) or (self.img.height() != h):
+            # self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+            self.canvas.itemconfig(self.image_handle, image=self.img)
+        self.canvas.update()
+        # self.paint()
+
+
+root = tk.Tk()
+# Resize the display window
+root.geometry("800x1000")
+app = Application(master=root)
+app.mainloop()
