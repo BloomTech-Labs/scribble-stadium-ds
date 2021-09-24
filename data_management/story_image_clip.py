07c98f4c3775e5aa83b3448b8644896730ba7128
Tasha Upchurch <TashaSkyUp@gmail.com>
2021-09-24 11:38:07 -0400

    Rename and refactor modules for consistent naming.

--- data_management/story_image_clip.py
+++ data_management/story_image_clip.py
@@ -1,23 +1,22 @@
+"""
+This modules purpose is to clip out inline images and other art surrounded by hand written text and save them
+"""
 
-import glob
-import os
-from enum import auto, IntFlag
+import numpy as np
+import os.path as path
 import tkinter as tk
 import cv2
-import numpy as np
-
-from data_management.phase_tkinter_class import PipelinePhase
+import os
+import glob
+from enum import IntFlag, auto
+from phase_tkinter_class import PipelinePhase
+from phase_tkinter_class import np_photo_image
 
 
 class Application(PipelinePhase):
-    """
-    This applications purpose is to clip out inline images and other art or writting errors surrounded by hand written
-    text and save them while removing them from tha photo of the text.
-    """
     def __init__(self, next_phase, master=None, prev_phase: PipelinePhase = None):
         super().__init__(next_phase, master=master, prev_phase=prev_phase)
-        self.phase = "phase1"
-        print(__name__)
+
 
         class States(IntFlag):
             choose_file = auto()
@@ -39,24 +38,20 @@ class Application(PipelinePhase):
         self.create_widgets()
 
     def create_widgets(self):
-        """
-        This function creates the widgets for the UI and default canvas widgets
-        :return: None
-        """
-        self.transform_btn = tk.Button(self.controls_frame)
+        self.transform_btn = tk.Button(self)
         self.transform_btn["text"] = "Clip"
         self.transform_btn["command"] = self.clip_button
         self.transform_btn.pack(side="top")
 
-        self.save_btn = tk.Button(self.controls_frame)
+        self.save_btn = tk.Button(self)
         self.save_btn["text"] = "save"
         self.save_btn["command"] = self.save_button
         self.save_btn.pack(side="top")
 
-        self.quit = tk.Button(self.controls_frame, text="QUIT", fg="red", command=self.destroy)
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         self.quit.pack(side="bottom")
 
-        self.next_phase_btn = tk.Button(self.controls_frame)
+        self.next_phase_btn = tk.Button(self)
         self.next_phase_btn["text"] = "Next Phase"
         self.next_phase_btn["command"] = self.next_phase_button
         self.next_phase_btn.pack(side="right")
@@ -66,30 +61,28 @@ class Application(PipelinePhase):
         self.image_handle = None
 
     def next_phase_button(self):
-        """
-        Sets a flag that helps in advancing to the next phase
-        :return: None
-        """
         self.goto_next_phase_flag = True
-        self.master.destroy()
+        command = self.master.destroy()
+
+    def save_button(self):
+        directory = path.dirname(self.filename)
+        filename, extension = path.basename(self.filename).split(".")
+        new_file_name = path.join(directory, filename + "-clipped" + "." + extension)
+        # convert before saving
+        self.np_img = np.array(cv2.cvtColor(self.np_img, cv2.COLOR_BGR2RGB))
+        cv2.imwrite(new_file_name, self.np_img)
+        # convert after saving so next phase gets correct image
+        self.np_img = np.array(cv2.cvtColor(self.np_img, cv2.COLOR_RGB2BGR))
+        self.filename=new_file_name
+        print(new_file_name)
 
     def get_next_clip_filename(self):
-        """
-        finds the next clip filename by searching for the last clip that was saved and incrementing the number on the
-        end of the file name.
-        """
-        directory = self.story_folder
-        tmp = os.path.join(directory, self.phase, "*-clip-*")
+        tmp = os.path.join(self.photo_folder, "*-clip-*")
         clips = glob.glob(tmp)
         name, ext = os.path.splitext(os.path.basename(self.filename))
 
         if clips == []:
-            try:
-                os.mkdir(os.path.join(self.story_folder, self.phase))
-            except FileExistsError as e:
-                self.phase_data_exists = True
-
-            ret_val = os.path.join(directory, self.phase, name + "-clip-00" + ext)
+            ret_val = os.path.join(self.photo_folder, name + "-clip-00" + ext)
             print(ret_val)
             return ret_val
         else:
@@ -102,14 +95,12 @@ class Application(PipelinePhase):
             else:
                 num = str(num)
 
-            retval = os.path.join(directory, self.phase, name + "-clip-" + num + ext)
+            retval = os.path.join(self.photo_folder, name + "-clip-" + num + ext)
             print(path, ext, num, retval)
             return retval
 
     def clip_button(self):
-        """
-        creates an actual clip attempt to preserve transparency
-        """
+
         def bounding_box(points):
             x_coordinates, y_coordinates = zip(*points)
 
@@ -149,14 +140,19 @@ class Application(PipelinePhase):
 
         file_name = self.get_next_clip_filename()
         cv2.imwrite(file_name, new_img)
-        self.redraw()
+        self.canvas.update()
+
+    def canvas_2_img_pt(self, canvas_pt: list):
+        img_x = canvas_pt[0] / self.canvas.winfo_width() * self.np_img.shape[1]
+        img_y = canvas_pt[1] / self.canvas.winfo_height() * self.np_img.shape[0]
+        return [img_x, img_y]
 
     def record_pt(self, canvas_pt: list):
         """
-        Record points and keep a track for the boundary box
-        :return: None
+        Record points the user has specified to the
         """
-        print("processing requested canvas space point:", canvas_pt)
+
+        print(self.np_img_points)
         img_x, img_y = self.canvas_2_img_pt(canvas_pt)
         self.np_img_points[self.current_np_img_point_idx] = [img_x, img_y]
         self.current_np_img_point_idx = self.current_np_img_point_idx + 1
@@ -164,13 +160,7 @@ class Application(PipelinePhase):
             self.state.remove(self.states.specify_points)
             self.state.add(self.states.modify_points)
 
-        print(self.np_img_points)
-
     def canvas_click(self, event):
-        """
-        Manages user input based on state
-        :return: None
-        """
         if self.states.specify_points in self.state:
             self.record_pt([event.x, event.y])
 
@@ -183,10 +173,7 @@ class Application(PipelinePhase):
                     self.canvas.coords(self.line_handles[pt1_idx], *(pt1 + pt2))
 
     def canvas_mouseover(self, event):
-        """
-        Provides the visual feedback to help user draw the boundary box around the text
-        :return: None
-        """
+
         ## move oval to cursor if needed
         if self.current_np_img_point_idx < 4:
             pt1 = [event.x, event.y]
@@ -215,11 +202,16 @@ class Application(PipelinePhase):
             self.canvas.coords(self.line_handles[2], [pt1[0], pt1[1], pt2[0], pt2[1]])
             self.canvas.coords(self.line_handles[3], [pt3[0], pt3[1], pt2[0], pt2[1]])
 
+    def img_2_canvas_pt(self, pt: list):
+        x = (pt[0] / self.np_img.shape[1]) * self.canvas.winfo_width()
+        y = (pt[1] / self.np_img.shape[0]) * self.canvas.winfo_height()
+        return ([x, y])
+
+
 
 if __name__ == "__main__":
-    """allows the phase to be ran in stand-alone mode or pipeline mode"""
     root = tk.Tk()
     # Resize the display window
     root.geometry("800x1000")
-    app = Application(master=root, next_phase=None)
+    app = Application(master=root,next_phase=None)
     app.mainloop()
