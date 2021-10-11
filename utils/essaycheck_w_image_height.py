e7fdc2fe56e7337a31cdc1a74dfd19d8291cb71c
Parvi Singh <72481455+singparvi@users.noreply.github.com>
2021-10-10 21:50:01 -0400

    improved and completed essaycheck_w_image_height.py to check for full_essays based on image height

--- utils/essaycheck_w_image_height.py
+++ utils/essaycheck_w_image_height.py
@@ -1,16 +1,48 @@
+import os
+
+import cv2
 from PIL import Image
 
-# Test using a full essay
-image = Image.open("/Users/rob/Downloads/starysquad-ground-truth-10-documents-summary/All_Data/Photo 3121.jpg")
-width, height = image.size
-# extract width and height from output tuple
 
-print(width, height)
+def full_essay_checker(directory, max_height):
+    """
+    :param directory:
+    :param max_height:
+    :return: None
+    This function will generate and display the files in the directory
+    passed in the arguments above by walking the tree and checking
+    if any of the .png, .tif, .jpg or .jpeg file in the directory
+    is not having too much height which may be the indicator of a full
+    child story that should not be publicly accessible due to child's safety.
+    """
+    # Check if the directory exists
+    if os.path.isdir(directory):
+        max_height = max_height  # This number can be changed in order to come up with
+        # Walk the directory
+        for root, directories, files in os.walk(directory):
+            for filename in files:
+                # Get file paths with specific extensions
+                if filename.endswith(".png") or filename.endswith(".tif") \
+                        or filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                    file_to_check = os.path.join(root, filename)
 
-# Test using a sample snippet
-image = Image.open("/Users/rob/GitHubProjects/scribble-stadium-ds/data/storysquad-ground-truth-51/5125/51-5125-1-0.png")
+                    # Get image dimensions
+                    image = Image.open(file_to_check)
+                    width, height = image.size
 
-width, height = image.size
-# extract width and height from output tuple
+                    # Check if the image height is more than the max_height
+                    # if it is then print file name:height in console and display to the user
+                    if height > max_height:
+                        print('Potential Full Essay Information (file:height shown) -->', file_to_check, height)
+                        # Display the image to the user
+                        img = cv2.imread(file_to_check)
+                        cv2.imshow('Potential Full Essay Image {Press any key to continue}', img)
+                        cv2.waitKey(0)
+                        cv2.destroyAllWindows()
+    else:
+        print('Directory not accessible!')
 
-print(width, height)
+
+# Run the function to find the files that do not have a corresponding txt files
+full_essay_checker(directory="../data",
+                   max_height=200)  # max_height can be changed for any other threshold
