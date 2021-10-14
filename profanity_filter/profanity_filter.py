a7cdb585448895a7fa90bb86c49d392b89dd50ba
Felicity Burris <sylviaf_ssanyu@yahoo.com>
2021-10-13 19:33:30 -0600

    'added global variables'

--- profanity_filter/profanity_filter.py
+++ profanity_filter/profanity_filter.py
@@ -3,3 +3,14 @@ import pandas as pd
 from profanity_filter import ProfanityFilter
 from profanity_check import predict, predict_prob
 
+#global variables
+filepath = '/content/Story 3121'
+transcriptions = {'images': [],
+          'metadata': []}
+df = pd.read_csv('bad_single.csv', usecols=[0], names=None)
+df2 = pd.read_csv('bad_phrases.csv', usecols=[0], names=None)
+bad_words = df['Bad_words'].to_list()
+bad_phrases = df2['Bad_phrases'].to_list()
+# combine lists
+bad_words_combined = bad_words + bad_phrases
+
