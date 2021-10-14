0b662a7427285ee80d9ed381831daf02ba5ba075
Felicity Burris <sylviaf_ssanyu@yahoo.com>
2021-10-13 21:12:27 -0600

    'changed name to _filters.py'

--- profanity_filter/profanity_filters.py
+++ profanity_filter/profanity_filters.py
@@ -1,46 +1,29 @@
 from json import loads, dumps
 import pandas as pd
-from profanity_filter import ProfanityFilter
+from profanity_filters import ProfanityFilter
 from profanity_check import predict, predict_prob
 
 #global variables
-filepath = '/content/Story 3121'
+filepath = 'profanity_filter/full_text.txt'
 transcriptions = {'images': [],
           'metadata': []}
-flagged_list = []
+transcriptions['images'].append(readFile(filename))
 df = pd.read_csv('bad_single.csv', usecols=[0], names=None)
+# load in bad phrases
 df2 = pd.read_csv('bad_phrases.csv', usecols=[0], names=None)
+# convert to list
 bad_words = df['Bad_words'].to_list()
 bad_phrases = df2['Bad_phrases'].to_list()
 # combine lists
 bad_words_combined = bad_words + bad_phrases
-
-# Function that removes punctuation from story
-def remove_punctuation(transcriptions):
-    parsed_string = dumps(transcriptions)
-    punctuations = '''[],!.'"\\?'''
-    for char in parsed_string:
-        if char in punctuations:
-            parsed_string = parsed_string.replace(char, '')
-    return parsed_string
+flagged_list = []
 
 
-# Function that looks for bad phrases in story
-def return_bad_phrases(transcriptions):
-    # Convert dict to str using dumps to keep phrases in tact
-    parsed_string = dumps(transcriptions)
-    # Lowercase to match list of bad phrases
-    parsed_string = parsed_string.lower()
-    # Remove punctuation
-    parsed_string = remove_punctuation(parsed_string)
-    # Returns list of matching words and puts in flagged_list global variable
-    for word in bad_phrases:
-        if word in parsed_string:
-            flagged_list.append(word)
-    # Returns dictionary with list of matches
-    dict = {'possible_words' : flagged_list}
-    return transcriptions.update(dict)
-
+def readFile(filepath):
+    fileObj = open(filepath, "r")  # opens the file in read mode
+    words = fileObj.read().replace('\n', ' ')  # puts the file into an array
+    fileObj.close()
+    return words
 
 # Function that looks for single bad words in story
 def return_bad_words(transcriptions):
@@ -60,3 +43,18 @@ def return_bad_words(transcriptions):
     dict = {'possible_words' : flagged_list}
     return transcriptions.update(dict)
 
+
+# Checks to see if any words have been added to the flagged_list
+def flag_bad_words(transcriptions):
+    if any(flagged_list):
+        dict = {'flagged' : [True]}
+        return transcriptions.update(dict)
+    else:
+        dict = {'flagged' : [False]}
+        return transcriptions.update(dict)
+
+# call functions on transcriptions
+print(return_bad_phrases(transcriptions))
+print(return_bad_words(transcriptions))
+# Scunthorpe Problem solved!
+print(transcriptions)
