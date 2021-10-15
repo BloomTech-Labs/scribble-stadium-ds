6d4d57aacf8810ae207d57e9ab320a4b859b2356
Felicity Burris <sylviaf_ssanyu@yahoo.com>
2021-10-14 18:01:39 -0600

    'added comments to the code'

--- profanity_word_phrase_filter/profanity_word_phrase_filters.py
+++ profanity_word_phrase_filter/profanity_word_phrase_filters.py
@@ -15,15 +15,23 @@ bad_phrases = df2['Bad_phrases'].to_list()
 
 
 def readFile(filepath):
-    fileObj = open(filepath, "r")  # opens the file in read mode
-    words = fileObj.read().replace('\n', ' ')  # puts the file into an array
+    """
+    :param filepath: inputs the text file of interest.
+    :returns: a string of words
+    This function opens the file in read mode, removes \n, which is created
+    when a string is created from text, and puts the file into an array"""
+    fileObj = open(filepath, "r")
+    words = fileObj.read().replace('\n', ' ')
     fileObj.close()
     return words
 
-transcriptions['images'].append(readFile(filepath))
-
 # Function that removes punctuation from story
 def remove_punctuation(transcriptions):
+    """
+    :param: transcriptions is the dictionary containing text file that has been
+    converted into an array.
+    :return: cleaned string of words
+    This function removes punctuations from the story """
     parsed_string = dumps(transcriptions)
     punctuations = '''[],!.'"\\?'''
     for char in parsed_string:
@@ -34,43 +42,55 @@ def remove_punctuation(transcriptions):
 
 # Function that looks for bad phrases in story
 def return_bad_phrases(transcriptions):
-    # Convert dict to str using dumps to keep phrases in tact
-    parsed_string = dumps(transcriptions)
-    # Lowercase to match list of bad phrases
-    parsed_string = parsed_string.lower()
-    # Remove punctuation
-    parsed_string = remove_punctuation(parsed_string)
+    """
+    :params transcriptions:transcriptions is the dictionary
+    containing text file that has been converted into an array
+    :return: updated transcriptions dictionary
+    This function searches for bad phrases in the story.it converts
+    a dictionary to str using dumps to keep phrases in tact. The
+    phrases are lowercased to match the list of bad phrases.the
+    punctuation is then removed. """
 
-    # Returns list of matching words and puts in flagged_list global variable
+    parsed_string = dumps(transcriptions)
+    parsed_string = parsed_string.lower()
+    parsed_string = remove_punctuation(parsed_string)
     for word in bad_phrases:
         if word in parsed_string:
             flagged_list.append(word)
-    # Returns dictionary with list of matches
     dict = {'possible_words' : flagged_list}
     return transcriptions.update(dict)
 
 
 # Function that looks for single bad words in story
 def return_bad_words(transcriptions):
-    # Parsing out just the story string from dict to avoid conflicts
+    """
+    :params transcriptions:transcriptions is the dictionary
+    containing text file that has been converted into an array
+    :return: updated transcriptions dictionary
+    This function searches for bad words in the story. It converts
+    a dictionary to str using dumps to keep words in tact. The
+    phrases are lowercased to match the list of bad phrases.the
+    punctuation is then removed. """
+
     parsed_string = list(transcriptions.values())[0][0]
-    # Lowercase to match list of bad words
     parsed_string = parsed_string.lower()
-    # Remove punctuation
     parsed_string = remove_punctuation(parsed_string)
-    # Splitting into list of strings to detect exact matches
     parsed_string = parsed_string.split()
-    # Finding matches and appending them to flagged_list
     for word in bad_words:
         if word in parsed_string:
             flagged_list.append(word)
-    # Returns dictionary with list of matches
     dict = {'possible_words' : flagged_list}
     return transcriptions.update(dict)
 
 
 # Checks to see if any words have been added to the flagged_list
 def flag_bad_words(transcriptions):
+    """
+    :params transcriptions:transcriptions is the dictionary
+    containing the text file that has been converted into an array
+    :return: updated transcriptions dictionary
+    This function checks to see if any words have been added to the flagged_list. """
+
     if any(flagged_list):
         dict = {'flagged' : [True]}
         return transcriptions.update(dict)
@@ -78,8 +98,9 @@ def flag_bad_words(transcriptions):
         dict = {'flagged' : [False]}
         return transcriptions.update(dict)
 
-# call functions on transcriptions
+
+transcriptions['images'].append(readFile(filepath))
 return_bad_phrases(transcriptions)
 return_bad_words(transcriptions)
-# Scunthorpe Problem solved!
+print(flag_bad_words(transcriptions))
 print(transcriptions)
