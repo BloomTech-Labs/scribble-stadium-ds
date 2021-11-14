5bb361cd788639de62a72f5b21765fbc1788928a
Tasha Upchurch <TashaSkyUp@gmail.com>
2021-11-14 15:54:01 -0500

    Corrected point tracking to track the corners of the writing area instead of the corners of the paper.

--- models/synthetic_data/synthetic_data_for_pipeline_transform/generate.py
+++ models/synthetic_data/synthetic_data_for_pipeline_transform/generate.py
@@ -2,6 +2,7 @@
 training data pipeline. """
 
 import random
+import sys
 import uuid
 import numpy
 import numpy as np
@@ -21,9 +22,10 @@ input_size = (256 - 64, 256)
 photo_sizes = [(2016, 1512, 3), (4032, 3024, 3), (640, 480, 3)]
 
 
-def create_fake_photo_unmodified(pix_per_mm: float, lines: int, red: [] = [255, 164, 164], blue: [] = [128, 128, 255],
-                                 ink: [] = [32, 32, 96], noise: float = 1):
-    """Creates an image that is much like written words on ruled paper"""
+def create_fake_paper_with_writing(pix_per_mm: float, lines: int = 25, red: [] = [255, 164, 164],
+                                   blue: [] = [128, 128, 255],
+                                   ink: [] = [32, 32, 96], noise: float = 1):
+    """Creates an image that is much like written words on ruled paper, also returns a dict of important information"""
 
     def get_rnd_col(col=[128, 128, 128], noise: int = 128):
         for i in range(len(col)):
@@ -42,6 +44,24 @@ def create_fake_photo_unmodified(pix_per_mm: float, lines: int, red: [] = [255, 
             col[i] = col[i] + mod
         return col
 
+    out_dict = {}
+
+    writing_area = {
+        "top_edge": 48 * pix_per_mm,
+        "left_edge": 32 * pix_per_mm,
+        "right_edge": 215.9 * pix_per_mm,
+        "bottom_edge": 279. * pix_per_mm
+    }
+
+    writing_area["verts_xy"] = [
+        [writing_area["left_edge"], writing_area["top_edge"]],
+        [writing_area["right_edge"], writing_area["top_edge"]],
+        [writing_area["right_edge"], writing_area["bottom_edge"]],
+        [writing_area["left_edge"], writing_area["bottom_edge"]],
+    ]
+
+    out_dict["writing_area"] = writing_area
+
     # randomize colors
     per_random = min(noise / 50.0, 1)
     red = get_rnd_col(col=red, noise=int(32 * per_random))
@@ -57,46 +77,50 @@ def create_fake_photo_unmodified(pix_per_mm: float, lines: int, red: [] = [255, 
                   dtype="uint8") * np.array(white).astype("uint8")
     print(img.shape)
 
-    def draw_box(box_corners, color: []):
+    def draw_box(box_corners, color: [],scale=True):
         """given the opposite corners of a box, draws a box in img of the specified color"""
         tmp = [box_corners[1], box_corners[3], box_corners[0], box_corners[2]]
         tmp = numpy.array(tmp)
-        tmp = (tmp * pix_per_mm).astype("uint")
+        if scale:
+            tmp = (tmp * pix_per_mm).astype("uint")
+        else:
+            tmp = tmp.astype("uint")
+
         img[tmp[0]:tmp[1], tmp[2]:tmp[3]] = color
 
     # draw blue lines, about 7.1 mm spacing between horizontal
     #
-    blue_lines_spacing = (paper_height_mm - (32. * 1.5)) / lines
+    blue_lines_spacing = (out_dict["writing_area"]["bottom_edge"] - out_dict["writing_area"]["top_edge"]) / lines
     for bl in range(lines):
         noise_val = random.random() * (noise / 200)
-        blue_lines_thickness_half = .25 + noise_val
+        blue_lines_thickness_half = .50 + noise_val
 
-        offset = (32 * 1.5) + (bl * blue_lines_spacing)
+        offset = out_dict["writing_area"]["top_edge"] + (bl * blue_lines_spacing)
         blue_line_box = [0,
                          offset - blue_lines_thickness_half,
-                         paper_width_mm,
+                         paper_width_mm*pix_per_mm,
                          offset + blue_lines_thickness_half
                          ]
-        draw_box(blue_line_box, blue)
+        draw_box(blue_line_box, blue,scale=False)
 
-    ## draw ink, this simulates where writing is on the paper
-    ink_lines_spacing = (paper_height_mm - (32. * 1.5)) / lines
+    # draw ink, this simulates where writing is on the paper
+    ink_lines_spacing = (out_dict["writing_area"]["bottom_edge"] - out_dict["writing_area"]["top_edge"]) / lines
 
     for il in range(lines):
-        ink_lines_offset = (32 * 1.5) + (il * ink_lines_spacing) + (.5 * ink_lines_spacing)
+        ink_lines_offset = out_dict["writing_area"]["top_edge"] + (il * ink_lines_spacing) + (.5 * ink_lines_spacing)
         noise_val = (random.random() - .5) * noise
         for x in range(int(32 * pix_per_mm), int(paper_width_mm * pix_per_mm)):
             # print("x: ", x, "/", int(paper_width_mm))
-            ymax = int((ink_lines_spacing + (ink_lines_spacing / 2.5)) * pix_per_mm)
+            ymax = int((ink_lines_spacing + (ink_lines_spacing / 2.5)) )
             for y in range(ymax):
 
-                yy = (ink_lines_offset) * pix_per_mm
+                yy = (ink_lines_offset)
                 probability_of_ink = (1 - (abs((y / ymax) - .5) * 2)) ** 8
                 probability_of_ink = probability_of_ink * .5 * ((1 - (noise / 100)) + (noise_val * .01))
                 if random.random() < probability_of_ink:
                     xx = x
                     tx = int(xx)
-                    ty = int(yy + y - (ink_lines_spacing * pix_per_mm / 2))
+                    ty = int(yy + y - (ink_lines_spacing  / 2))
                     if ty < img.shape[0]:
                         img[ty, tx] = ink
 
@@ -117,11 +141,14 @@ def create_fake_photo_unmodified(pix_per_mm: float, lines: int, red: [] = [255, 
     draw_box([0, paper_height_mm - 3, paper_width_mm, paper_height_mm], [128, 128, 128])  # bottom
     draw_box([paper_width_mm - 3, 0, paper_width_mm, paper_height_mm], [128, 128, 128])  # right
     draw_box([0, 0, 3, paper_height_mm], [128, 128, 128])  # left
-    return img
+    return img, out_dict
 
 
-def randomize_photo_transform(img, photo_size, noise=1):
-    """takes a generated sample and transforms it to mimic the appearance of a photo taken by a camera."""
+def randomize_photo_transform(img, img_info, photo_size, noise=1):
+    """takes a generated sample and transforms it to mimic the appearance of a photo taken by a camera.
+    returns the modified image and the set of points
+    """
+
     img_w = img.shape[1]
     img_h = img.shape[0]
     photo_h = photo_size[0]
@@ -140,10 +167,17 @@ def randomize_photo_transform(img, photo_size, noise=1):
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     cv2.warpPerspective(img, matrix, dsize=(photo_w, photo_h), dst=photo_img, borderMode=cv2.BORDER_TRANSPARENT)
 
-    return photo_img, pts2
+    writing_pts = img_info["writing_area"]["verts_xy"]
+
+    out_writing_pts = []
+    for pt in writing_pts:
+        src = np.array([[[pt[0], pt[1]]]]).astype("float32")
+        out_writing_pts.append( (cv2.perspectiveTransform(src, matrix) - 1).reshape((2)))
+
+    return photo_img, out_writing_pts
 
 
-def create_one(noise: float = 1, set_name: str = ""):
+def create_one_and_save_it(noise: float = 1, set_name: str = ""):
     """
     function to create a useful sample and save it to the local disk, given an amount of noise and a name for the
     set that it is part of
@@ -158,11 +192,13 @@ def create_one(noise: float = 1, set_name: str = ""):
 
     os.makedirs(os.path.dirname(file_name_img_X_input), exist_ok=True)
 
-    img = create_fake_photo_unmodified(5, 25, noise=noise)
-    img2, pts = randomize_photo_transform(img, photo_sizes[0], noise=noise)
+    img, img_info = create_fake_paper_with_writing(5, 25, noise=noise)
+    img2, pts = randomize_photo_transform(img, img_info, photo_sizes[0], noise=noise)
     img3 = cv2.resize(img2, input_size, interpolation=cv2.INTER_AREA)
     scaleX = input_size[0] / photo_sizes[0][1]
     scaleY = input_size[1] / photo_sizes[0][0]
+    typer.echo(pts)
+    pts = np.array(pts)
     pts[:, 0] = pts[:, 0] * scaleX
     pts[:, 1] = pts[:, 1] * scaleY
     img3 = cv2.cvtColor(img3, cv2.COLOR_RGB2BGR)
@@ -188,7 +224,7 @@ def create_bunch(how_many: int, noise: float = 1, cores: int = 4, noise_start: i
     """function to create many synthetic samples, given a noise level and how many to create."""
     # creates a list of processes waiting to be started and completed.
     if noise_start == -1:
-        processes = [Process(target=create_one, args=(noise, "" + str(noise))) for i in
+        processes = [Process(target=create_one_and_save_it, args=(noise, "" + str(noise))) for i in
                      range(0, how_many)]
     else:
         processes = []
@@ -199,11 +235,11 @@ def create_bunch(how_many: int, noise: float = 1, cores: int = 4, noise_start: i
         for current_noise in range(noise_start, noise_end, noise_step):
             for sample_i in range(samples_per_step):
                 processes.append(
-                    Process(target=create_one, args=(current_noise, "" + str(current_noise)))
+                    Process(target=create_one_and_save_it, args=(current_noise, "" + str(current_noise)))
                 )
 
     # start the initial group
-    for i in range(cores):
+    for i in range(min(cores, len(processes))):
         p = processes[i]
         p.start()
 
@@ -245,4 +281,8 @@ if __name__ == "__main__":
     print(os.path.dirname(__file__))
     cores_to_use = max(os.cpu_count() - 2, 1)
     cores_to_use = min(cores_to_use, 12)
-    typer.run(create_bunch)
+
+    if int(sys.argv[1]) > 1:
+        typer.run(create_bunch)
+    else:
+        create_one_and_save_it(noise=25)
