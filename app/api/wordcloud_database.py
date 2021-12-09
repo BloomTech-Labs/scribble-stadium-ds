09d3dd6f41236e82b11aa8ef40740843bdb20579
afroman32 <makoanoble@gmail.com>
2021-12-09 16:56:08 -0600

    satisfy linter

--- app/api/wordcloud_database.py
+++ app/api/wordcloud_database.py
@@ -12,29 +12,29 @@ load_dotenv()
 router = APIRouter()
 
 
-#connect to ElephantSQL-hosted PostgreSQL
+# connect to ElephantSQL-hosted PostgreSQL
 DB_NAME = os.getenv("RDS_DB_NAME", default="OOPS")
 DB_USER = os.getenv("RDS_USERNAME", default="OOPS")
 DB_PASSWORD = os.getenv("RDS_PASSWORD", default="OOPS")
 DB_HOST = os.getenv("RDS_HOSTNAME", default="OOPS")
 DB_PORT = os.getenv("RDS_PORT", default="OOPS")
 
-DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(
-    DATABASE_URL, pool_pre_ping=True
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 Base.metadata.create_all(bind=engine)
 
+
 class Stories(Base):
     __tablename__ = "stories"
 
     id = Column(Integer, primary_key=True, index=True)
     story = Column(String, index=True)
+
 
 def get_db():
     try:
@@ -43,14 +43,18 @@ def get_db():
     finally:
         db.close()
 
+
 def get_story(db: Session, id: int):
     return db.query(Stories).filter(Stories.id == id).first()
 
-@router.get('/storytext')
+
+@router.get("/storytext")
 def show_story(id: int = 1, numWords: int = 20, db: Session = Depends(get_db)):
     db_story = get_story(db, id=id)
     if db_story is None:
-        raise HTTPException(status_code=404, detail="Story not found, id must be between 1-167")
+        raise HTTPException(
+            status_code=404, detail="Story not found, id must be between 1-167"
+        )
 
     story_string = db_story.story
 
