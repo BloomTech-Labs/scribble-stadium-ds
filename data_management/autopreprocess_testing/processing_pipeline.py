e1c1830db8ec15f5b40916a0cb50675a0eb1362b
Carl Stimson <carlstimson@gmail.com>
2021-12-21 13:04:38 +0900

    Fixes bugs in processing pipeline

--- data_management/autopreprocess_testing/processing_pipeline.py
+++ data_management/autopreprocess_testing/processing_pipeline.py
@@ -1,4 +1,4 @@
-from auto_preprocess import *
+from preprocessing_functions import *
 
 def processing_pipeline(img):
     """
@@ -16,6 +16,6 @@ def processing_pipeline(img):
     newImage = img.copy()
     img_grayscaled = get_grayscale(newImage)
     img_denoised = remove_noise(img_grayscaled)
-    image_G_thresh = adaptiveGaussianThresholding(get_grayscale(img_denoised), blockSize=5, c=2)
+    image_G_thresh = adaptiveGaussianThresholding(img_denoised, blockSize=5, c=2)
     img_eroded = erode(image_G_thresh, kernelSize=2)
     return img_eroded
