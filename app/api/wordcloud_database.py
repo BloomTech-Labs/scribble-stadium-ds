34e9c7c9c01e0d35e68f297d358e3830c686974e
afroman32 <afroman32@users.noreply.github.com>
2022-01-12 23:18:05 -0600

    Fixed bug so the StoryText API endpoint is usable (#96)
    * update wordcloud_function.py and wordcloud_database.py so the storytext endpoint will work
    
    * clean up files and add comments
    
    * satisfy linter
    
    * update filepath for complex_words.csv
    
    * satisfy linter
    
    * satisfy linter
    
    * remove scribble folder
    
    * fix filepath for complex_words.csv
    
    * reformat for readability
    
    * reformat for readability

--- app/api/wordcloud_database.py
+++ app/api/wordcloud_database.py
@@ -1,70 +1,65 @@
-# from sqlalchemy import create_engine, Integer, Column, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker, Session
-# from fastapi import APIRouter, Depends, HTTPException
-# import os
-# from dotenv import load_dotenv
-# from app.utils.wordcloud.wordcloud_functions import clean_text, complexity_df
+from sqlalchemy import create_engine, Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi import APIRouter, Depends, HTTPException
+import os
+from dotenv import load_dotenv
+from app.utils.wordcloud.wordcloud_functions import complexity_df
 
 
-# load_dotenv()
+load_dotenv()
 
-# router = APIRouter()
+router = APIRouter()
 
 
-# #connect to ElephantSQL-hosted PostgreSQL
-# DB_NAME = os.getenv("RDS_DB_NAME", default="OOPS")
-# DB_USER = os.getenv("RDS_USERNAME", default="OOPS")
-# DB_PASSWORD = os.getenv("RDS_PASSWORD", default="OOPS")
-# DB_HOST = os.getenv("RDS_HOSTNAME", default="OOPS")
-# DB_PORT = os.getenv("RDS_PORT", default="OOPS")
+# connect to ElephantSQL-hosted PostgreSQL
+DB_NAME = os.getenv("RDS_DB_NAME", default="OOPS")
+DB_USER = os.getenv("RDS_USERNAME", default="OOPS")
+DB_PASSWORD = os.getenv("RDS_PASSWORD", default="OOPS")
+DB_HOST = os.getenv("RDS_HOSTNAME", default="OOPS")
+DB_PORT = os.getenv("RDS_PORT", default="OOPS")
 
-# DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# engine = create_engine(
-#     DATABASE_URL, pool_pre_ping=True
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base = declarative_base()
+Base = declarative_base()
 
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
-# class Stories(Base):
-#     __tablename__ = "stories"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     story = Column(String, index=True)
+class Stories(Base):
+    __tablename__ = "stories"
 
-# def get_db():
-#     try:
-#         db = SessionLocal()
-#         yield db
-#     finally:
-#         db.close()
+    id = Column(Integer, primary_key=True, index=True)
+    story = Column(String, index=True)
 
-# def get_story(db: Session, id: int):
-#     return db.query(Stories).filter(Stories.id == id).first()
 
-# @router.get('/storytext')
-# def show_story(id: int = 1, db: Session = Depends(get_db)):
-#     db_story = get_story(db, id=id)
-#     if db_story is None:
-#         raise HTTPException(status_code=404, detail="Story not found, id must be between 1-167")
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
-#     story_string = db_story.story
 
-#     story_words = clean_text(story_string)
+def get_story(db: Session, id: int):
+    return db.query(Stories).filter(Stories.id == id).first()
 
-#     words = complexity_df(story_words)
 
-#     #Use length count metric=
-#     words['len'] = words['word'].apply(len)
-#     words['complexity'] = words['len'] / words['count']
+@router.get("/storytext")
+def show_story(id: int = 1, numWords: int = 20, db: Session = Depends(get_db)):
+    db_story = get_story(db, id=id)
+    if db_story is None:
+        raise HTTPException(
+            status_code=404, detail="Story not found, id must be between 1-167"
+        )
 
-#     # scale the complexities so the sum is 1000
-#     words['complexity'] = words['complexity'] / words['complexity'].sum()
-#     word_complexities = dict(zip(words.word, words.complexity))
+    story_string = db_story.story
 
-#     return word_complexities
+    # sends the string of the story and the number of words we want the complexity for
+    words = complexity_df(story_string, numWords)
+
+    return words
 
