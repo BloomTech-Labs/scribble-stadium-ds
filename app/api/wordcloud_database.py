44a640c214e2419ed0a22ce87291f6fe320c7484
Niger Little-Poole <nl2418@columbia.edu>
2022-01-14 17:09:05 -0400

    Fix up dev environment (#105)
    * Edit dockerfile to support multi stage build
    
    * Edit tesseract dockerfile to support multi stage build
    
    * Include docker train image updates from Niger
    
    * Update docker commands
    
    * update Docker file names
    
    * update comments to reflect file changes
    
    * Update Dockerfile and docker-compose.yml with requested changes from Niger
    
    * update main readme with new file names
    
    * update readme again
    
    * Final Tweaks to Docker Commands for API
    
    * initial tweaks
    
    * fix api wordcloud issue by mocking over sqlalchemy, ignore model files, and introduce updated README
    
    * updated documentation and gitignore
    
    * check all rds variables and raise warning if not all present
    
    Co-authored-by: masonmenges <mason.menges2@gmail.com>
    Co-authored-by: NLP <nlp@constellationanalytics.com>
    Co-authored-by: Ashalesh Tilawat <73544020+ashtilawat23@users.noreply.github.com>

--- app/api/wordcloud_database.py
+++ app/api/wordcloud_database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Integer, Column, String
+from sqlalchemy import create_engine, Integer, Column, String, create_mock_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi import APIRouter, Depends, HTTPException
@@ -13,15 +13,23 @@ router = APIRouter()
 
 
 # connect to ElephantSQL-hosted PostgreSQL
-DB_NAME = os.getenv("RDS_DB_NAME", default="OOPS")
-DB_USER = os.getenv("RDS_USERNAME", default="OOPS")
-DB_PASSWORD = os.getenv("RDS_PASSWORD", default="OOPS")
-DB_HOST = os.getenv("RDS_HOSTNAME", default="OOPS")
-DB_PORT = os.getenv("RDS_PORT", default="OOPS")
+DB_NAME = os.getenv("RDS_DB_NAME", default=None)
+DB_USER = os.getenv("RDS_USERNAME", default=None)
+DB_PASSWORD = os.getenv("RDS_PASSWORD", default=None)
+DB_HOST = os.getenv("RDS_HOSTNAME", default=None)
+DB_PORT = os.getenv("RDS_PORT", default=None)
+
+
+def dump(sql, *multiparams, **params):
+    print(sql.compile(dialect=engine.dialect))
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = None
+if all([DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT]):
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+else:
+    print("Warning: Not all RDS environment variables set, falling back to mock database")
+    engine = create_mock_engine("postgresql://mock:{mock}@mock:5432/mock", dump)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
