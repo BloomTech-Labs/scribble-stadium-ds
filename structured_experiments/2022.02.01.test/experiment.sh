da99e1610186657d93038e64d7f0d3940093f5a2
NLP <nlp@constellationanalytics.com>
2022-02-08 17:08:06 -0500

    did a little cleanup

--- structured_experiments/2022.02.01.test/experiment.sh
+++ structured_experiments/2022.02.01.test/experiment.sh
@@ -1,4 +1,4 @@
-echo 'Setting up kaggle test data for tesseract training'
+echo 'Setting Up Kaggle Test Data for Tesseract Training'
 python setup_ground_truth.py /train/tesstrain/data/kaggle-ground-truth
 echo 'Training Kaggle Test Model'
 make training MODEL_NAME=kaggle START_MODEL=eng TESSDATA=/train/tessdata
