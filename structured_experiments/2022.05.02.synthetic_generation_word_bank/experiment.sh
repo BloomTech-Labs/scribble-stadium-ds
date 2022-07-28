5efc6f5b0388beb857202613c6090ee2aae327d1
Yeraldina <67125251+yestrella14@users.noreply.github.com>
2022-07-27 21:48:18 -0400

    Bl 324 as a developer i need a word bank to generate data (#131)
    * My first commit
    
    * BL-324 Word Bank for sythetic data generation
    
    * Second commit
    
    * Fixed white spacing
    
    * Adding one Line spacing

---
+++ structured_experiments/2022.05.02.synthetic_generation_word_bank/experiment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Setting Up Synthetic Test Data for Tesseract Training"
+mkdir -p /train/tesstrain/data/synthetic-ground-truth
+python synthetic_data_generator.py /train/tesstrain/data/synthetic-ground-truth
+echo 'Training Synthetic Test Model'
+make training MODEL_NAME=synthetic START_MODEL=eng TESSDATA=/train/tessdata
+echo 'Finished'
